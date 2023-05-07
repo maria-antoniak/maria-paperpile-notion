94b374a2935d37a7d62bc1d2980a689700fda1a3
Maria Antoniak <maria-antoniak@users.noreply.github.com>
2023-05-06 21:15:13 -0700

    Update sync.py

--- sync.py
+++ sync.py
@@ -15,151 +15,111 @@ ARCHIVE_PATH = 'archive.pk'
 BIB_PATH = 'references.bib'
 NOTION_TOKEN = os.environ['NOTION_TOKEN']
 DATABASE_IDENTIFIER = os.environ['DATABASE_IDENTIFIER']
+HEADERS = {"Accept": "application/json",
+           "Notion-Version": "2022-06-28",
+           "Content-Type": "application/json",
+           "Authorization": f"Bearer {NOTION_TOKEN}"}
 
 
-def notion_add_entry(
-    title='',
-    authors='',
-    year='0',
-    ref_id='',
-    link='',
-):
+def get_payload(title='',
+                authors='',
+                year='',
+                ref_id='',
+                link='',
+                abstract='',
+                keywords=''):    
+
+    payload = {
+        "parent": {
+            'database_id': DATABASE_IDENTIFIER,
+        },
+        "properties": {
+            'Title': {
+                'title': [{
+                    'text': {
+                        'content': title,
+                        }
+                    }]
+                },
+            'Reference ID': {
+                "rich_text": [{
+                    "type": "text",
+                    "text": {
+                        "content": ref_id,
+                    }
+                }],
+            }   
+        }
+    }
+
+    if authors:
+        payload["properties"]['Authors'] = {"rich_text": [{"type": "text",
+                                                           "text": {"content": authors}}]}
+
+    if abstract:
+        payload["properties"]['Abstract'] = {"rich_text": [{"type": "text",
+                                                            "text": {"content": abstract}}]}
+
+    if keywords:
+        formatted_tags = [{"name": _tag} for _tag in keywords.split(';')]
+        payload["properties"]["Tags"] = {"multi_select": formatted_tags}
+    else:
+        payload["properties"]["Tags"] = {"multi_select": []}
+    
+    if link:
+        payload["properties"]["Link"] = {"url": link}
+
+    if year:
+        payload["properties"]['Year'] = {"rich_text": [{"type": "text",
+                                                        "text": {"content": year}}]}
+
+    return payload
+
+
+
+def notion_add_entry(title='',
+                     authors='',
+                     year='0',
+                     ref_id='',
+                     link='',
+                     abstract='',
+                     keywords=''):
+    
     url = "https://api.notion.com/v1/pages"
-    payload = {
-        "parent": {
-            'database_id': DATABASE_IDENTIFIER,
-        },
-        "properties": {
-            'Title': {
-                'title': [{
-                    'text': {
-                        'content': title,
-                        }
-                    }]
-                },
-            'Authors': {
-                "rich_text": [{
-                    "type": "text",
-                    "text": {
-                        "content": authors,
-                    }
-                }],
-            },
-            'Year': {
-                "rich_text": [{
-                    "type": "text",
-                    "text": {
-                        "content": year,
-                    }
-                }],
-            },
-            'Reference ID': {
-                "rich_text": [{
-                    "type": "text",
-                    "text": {
-                        "content": ref_id,
-                    }
-                }],
-            },
-            'Link': {
-                "url": link,
-            },
-                    
-                    
-        },
-    }
-    #  pprint.pprint(payload)
-    headers = {
-        "Accept": "application/json",
-        "Notion-Version": "2022-06-28",
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {NOTION_TOKEN}"
-    }
-    response = requests.post(url, json=payload, headers=headers)
-    #  pprint.pprint(json.loads(response.text))
+    payload = get_payload(title, authors, year, ref_id, link, abstract, keywords)
+    # pprint.pprint(payload)
+
+    response = requests.post(url, json=payload, headers=HEADERS)
+    # pprint.pprint('inside notion_add_entry')
+    # pprint.pprint(json.loads(response.text))
 
 
-def notion_update_page(
-    page_id,
-    title='',
-    authors='',
-    year='0',
-    ref_id='',
-    link='',
-):
+def notion_update_page(page_id,
+                       title='',
+                       authors='',
+                       year='0',
+                       ref_id='',
+                       link='',
+                       abstract='',
+                       keywords=''):
+    
     url = f"https://api.notion.com/v1/pages/{page_id}"
-    payload = {
-        "parent": {
-            'database_id': DATABASE_IDENTIFIER,
-        },
-        "properties": {
-            'Title': {
-                'title': [{
-                    'text': {
-                        'content': title,
-                        }
-                    }]
-                },
-            'Authors': {
-                "rich_text": [{
-                    "type": "text",
-                    "text": {
-                        "content": authors,
-                    }
-                }],
-            },
-            'Year': {
-                "rich_text": [{
-                    "type": "text",
-                    "text": {
-                        "content": year,
-                    }
-                }],
-            },
-            'Reference ID': {
-                "rich_text": [{
-                    "type": "text",
-                    "text": {
-                        "content": ref_id,
-                    }
-                }],
-            },
-            'Link': {
-                "url": link,
-            },
-                    
-                    
-        },
-    }
-    #  pprint.pprint(payload)
-    headers = {
-        "Accept": "application/json",
-        "Notion-Version": "2022-06-28",
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {NOTION_TOKEN}"
-    }
-    response = requests.patch(url, json=payload, headers=headers)
+    payload = get_payload(title, authors, year, ref_id, link, abstract, keywords)
+    # pprint.pprint(payload)
+    response = requests.patch(url, json=payload, headers=HEADERS)
+    # pprint.pprint('inside notion_update_page')
+    # pprint.pprint(json.loads(response.text))
 
 
 def notion_fetch_page(ref_id):
     url = f"https://api.notion.com/v1/databases/{DATABASE_IDENTIFIER}/query"
 
-    # list database pages
-    payload = {
-        "page_size": 1,
-        "filter": {
-            'property': 'Reference ID',
-            'rich_text': {"equals": ref_id},
-        },
-    }
-    headers = {
-        "Accept": "application/json",
-        "Notion-Version": "2022-06-28",
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {NOTION_TOKEN}"
-    }
+    # List database pages
+    payload = { "page_size": 1,
+                "filter": {'property': 'Reference ID',
+                           'rich_text': {"equals": ref_id}}}
     
-    response = requests.post(url, json=payload, headers=headers)
+    response = requests.post(url, json=payload, headers=HEADERS)
     
     response = json.loads(response.text)
     #  pprint.pprint(response)
@@ -200,9 +160,22 @@ def clean_str(string):
     return string
 
 
+def format_authors(test_string):
+    authors = [a.split(',') for a in test_string.split(';')]
+    formatted_authors = [] 
+    for a in authors:
+        if len(a) == 1:
+            formatted_authors.append(a[0].strip())
+        elif len(a) == 2:
+            formatted_authors.append(a[1].strip() + ' ' + a[0].strip())
+        else:
+            formatted_authors.append(' '.join(a).strip())
+    return ', '.join(formatted_authors)
+
+
 def main():
 
-    # instantiate the parser
+    # Instantiate the parser
     parser = bibtexparser.bparser.BibTexParser()
     parser.ignore_nonstandard_types = True
     parser.homogenize_fields = False
@@ -218,7 +191,7 @@ def main():
         archive = []
     archive_ids = [e['ID'] for e in archive]
 
-    # add each entry to notion database
+    # Add each entry to notion database
     update_archive = False
     for entry in reversed(bibliography.entries):
 
@@ -228,36 +201,51 @@ def main():
 
         authors = entry.get('author', '')
         authors = authors.replace(' and ', '; ')
+        authors = authors.replace(' And ', '; ')
         authors = clean_str(authors)
+        authors = format_authors(authors)
 
         year = entry.get('year', '')
         link = entry.get('url', '')
         ref_id = entry.get('ID')
+        
+        abstract = entry.get('abstract', '')
+        
+        keywords = entry.get('keywords', '')
 
-        if ref_id not in archive_ids: # new page
-            notion_add_entry(
-                title=title,
-                authors=authors,
-                year=year,
-                ref_id=ref_id,
-                link=link,
-            )
+        # Create new page
+        if ref_id not in archive_ids:
+#             pprint.pprint('ref_id not in archive_ids')
+#             pprint.pprint(ref_id)
+#             pprint.pprint(archive_ids)
+            notion_add_entry(title=title,
+                             authors=authors,
+                             year=year,
+                             ref_id=ref_id,
+                             link=link,
+                             abstract=abstract,
+                             keywords=keywords)
             update_archive = True
-        elif entry not in archive: # update existing page
+
+         # Update existing page
+        elif entry not in archive:
+            # pprint.pprint('entry not in archive')
             page_id = notion_fetch_page(ref_id)
             if page_id != -1:
-                notion_update_page(
-                    page_id=page_id,
-                    title=title,
-                    authors=authors,
-                    year=year,
-                    ref_id=ref_id,
-                    link=link,
-                )
+                # pprint.pprint('page_id != -1')
+                notion_update_page(page_id=page_id,
+                                   title=title,
+                                   authors=authors,
+                                   year=year,
+                                   ref_id=ref_id,
+                                   link=link,
+                                   abstract=abstract,
+                                   keywords=keywords)
                 update_archive = True
 
-    # only update the archive if necessary
+    # Only update the archive if necessary
     if update_archive:
+        pprint.pprint('update_archive')
         with open(ARCHIVE_PATH, 'wb') as archive_file:
             archive = pickle.dump(bibliography.entries, archive_file)
 
